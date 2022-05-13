af2f1a50cb0ddb516a3841dee01ffed9eeb19d5e
Ezy93 <giordani001@gmail.com>
2022-05-13 17:40:58 +0200

    modificato tipo di dato per la data e modificato nome della colonna per la data di arrivo

--- database/migrations/2022_05_13_144745_create_trains_table.php
+++ database/migrations/2022_05_13_144745_create_trains_table.php
@@ -18,8 +18,8 @@ class CreateTrainsTable extends Migration
             $table->string('company', 50);
             $table->string('departure_station', 50);
             $table->string('arrival_station', 50);
-            $table->dateTime('departure_date');
-            $table->dateTime('arrival_hour');
+            $table->date('departure_date');
+            $table->date('arrival_date');
             $table->string('train_code', 10);
             $table->tinyInteger('number_of_carriages');
             $table->boolean('isInTime');
--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -3,5 +3,7 @@
 @section('title','trains')
 
 @section('main-content')
-    
+    <main>
+        
+    </main>
 @endsection

0f71b6ecba2ba16e5c987f4d95be0b811e4a1dda
Ezy93 <giordani001@gmail.com>
2022-05-13 18:09:17 +0200

    inseriti dati di tutti i train in delle card per essere visualizzati sullo schermo

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -4,6 +4,38 @@
 
 @section('main-content')
     <main>
-        
+        <div class="container-fluid">
+            <div class="row">
+                
+                    @foreach ($trains as $train)
+                        <div class="col-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title"><span>Company:</span>{{' '.$train->company}}</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted"><span>Train-code:</span>{{' '.$train->train_code}}</h6>
+                                    <p class="card-text"><span>Departure Station:</span>{{' '.$train->departure_station}}</p>
+                                    <p class="card-text"><span>Arrival Station:</span>{{' '.$train->arrival_station}}</p>
+                                    <p class="card-text"><span>Departure:</span>{{' '.$train->departure_date}}</p>
+                                    <p class="card-text"><span>Arrival:</span>{{' '.$train->arrival_date}}</p>
+                                    <p class="card-text"><span>Carriages:</span>{{' '.$train->number_of_carriages}}</p>
+                                    <p class="card-text">
+                                        @if (!$train->isDelete)
+                                            @if ($train->isInTime)
+                                                <span>il treno arriverà in orario</span>
+                                            @else
+                                                <span>il treno ha subito un leggero ritardo</span>
+                                            @endif
+                                        @else
+                                            <span>Siamo spiacenti ma il treno è stato cancellato</span>
+                                        @endif
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                
+            </div>
+
+        </div>
     </main>
 @endsection
